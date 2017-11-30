372ad3f0d515313d62cf4581b89eb7829f3c25cf
jhojanmll <jhojanmll@gmail.com>
2017-11-30 04:37:57 -0500

    habitacion

---
+++ habitaciones/habitaciontriple.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/habitacion.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container fondo">
+        <div class="diseñohabitacion">
+            
+        <div class="row">
+            <div class="col-md-7 ">
+              
+                <img src="../img/habitaciontriple.jpg" alt="foto de esta habitacion" class="tamañoimagen">
+            </div>
+            <div class="col-md-5">
+            <i class="fa fa-shower" aria-hidden="true">Baño privado</i>
+            <div class="info">
+                
+                <strong>        
+                Tamaño de la habitacion
+                </strong> =
+                "
+                25 m2
+                "
+
+            </div>
+            <p>  
+            Habitación triple con aire acondicionado y TV por cable.
+            </p>  
+        
+            <strong>
+             Equipamiento de la habitacón:
+            </strong>  
+            <p class="text-justify">  
+               Ducha, TV, Articulos de aseo gratis, Ventilador, Baño, Calefaccion, Canales cable,Toallas/Sabanas (con suplemento), Armario, Perchero, Papeleras, Almohada hipoalergenica.
+                 <p>
+                     Wifi gratis!
+                 </p>       
+            </p>            
+        </div>
+        </div>
+        
+
+    </div>
+    
+</body>
+</html>

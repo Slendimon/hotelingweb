15a387ace7706c17e42b14a60d6cf02f3a80a019
Slendimon <pdassem@gmail.com>
2017-12-04 12:24:38 -0500

    nothing to show

--- php/reservas.php
+++ php/reservas.php
@@ -25,7 +25,7 @@
                     <ul>
                         <li><a href="../index.html#Servicios">SERVICIOS</a></li>
                         <li><a href="../index.html#habitaciones">HABITACIONES</a></li>
-                        <li><a href="contactos.php">CONTACTO</a></li>
+                        <li><a href="../index.html#contacto.php">CONTACTO</a></li>
                         <li class="reserva"><a href="reservas.php">RESERVAR</a></li>
                         <li><a href="login.html">LOGIN</a></li>
                     </ul>

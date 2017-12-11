76a7472ceeda0dc59abfe2c27f2f4a7cd308248c
Slendimon <pdassem@gmail.com>
2017-12-10 21:13:11 -0500

    actualizado

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    
+        $("#banner1").css({"height":$(window).height() + "px"});
+    
+        var flag = false;
+        var scroll;
+    
+        $(window).scroll(function(){
+            scroll = $(window).scrollTop();
+    
+            if(scroll > 200){
+                if(!flag){
+                    
+                    $("header").css({"background-color": "#3C3C3C"});
+                    $("header nav ul li a").css({"color":"#FFFFFF"});
+                    $("nav ul li a:hover").css({"color": "#D8D8D8"});
+                    flag = true;
+                }
+            }else{
+                if(flag){
+                    //$("#logo").css({"margin-top": "150px", "width": "250px","height":"250px"});
+    
+                    $("header").css({"background-color": "#FFFFFF"});
+                    $("header nav ul li a").css({"color":"#3C3C3C"});
+                    flag = false;
+                }
+            }
+    
+    
+        });
+    
+    });
